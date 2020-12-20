ff5a6071a6f60217460a98e044dc99d42885721d
aron <admin@lepuri.com>
2020-12-20 15:12:16 +0100

    sami primer commit

---
+++ about.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Las 3 metaetiquetas anteriores * deben * ser lo primero en la cabeza; cualquier otro contenido principal debe venir * después * de estas etiquetas -->
+  <title>Lepuri kod</title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet" />
+
+</head>
+
+<body>
+<?php include"menu.html" ?>
+ <section id="about">
+  <div class="about">
+      <div class="text-center">
+        <h2>Quienes somos</h2>
+        <div class="col-md-10 col-md-offset-1">
+          <p style="color: black;">LepuriKod es una empresa enfocada en el nicho de la programacion el marketing y el diseño</p>
+          <h1>Que hacemos?</h1>
+          <p>Realizamos webs desde cero con lenguajes de programacion Como mencionaremos luego, El cliente tendra libre derecho de realizar su pagina como desee dentro de las posibilidades, Trabajamos con base de datos, Las tiendas webs por ejemplo necesitan base de datos para cargar sus productos,Ademas contamos con diseñadora grafica la cual realiza trabajos como logos,tarjetas personales,Boda etc, Si ya tienes tu web pero no sabes donde alojarla, contamos con hosting y dominio con soporte 24/7 Para tener prencencia en linea.</p>
+        </div>
+      </div>
+      <div class="services">
+    <div class="container">
+      <div class="text-center">
+        <h1 style="color: black">Especialistas</h1>
+        <div class="col-md-3 wow fadeInDown" data-wow-duration="500ms" data-wow-delay="300ms">
+          <i class="fa fa-heart-o" style="color: #222831;"></i>
+          <h1>Programadores web</h1>
+          <p>Son los encargados de hechar codigo a tu web, Aran la parte mas importante que sera redactar </p>
+        </div>
+        <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <i class="fa fa-cloud" style="color: #222831;"></i>
+          <h3>Retina Ready</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum nam numquam voluptates cumque inventore, quibusdam corporis consequatur amet.</p>
+        </div>
+        <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <i class="fa fa-book" style="color: #222831;"></i>
+          <h3>Fresh and Clean</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum nam numquam voluptates cumque inventore, quibusdam corporis consequatur amet.</p>
+        </div>
+        <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms">
+          <i class="fa fa-gear" style="color: #222831;"></i>
+          <h3>Easy to Customize</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum nam numquam voluptates cumque inventore, quibusdam corporis consequatur amet.</p>
+        </div>
+      </div>
+    </div>
+    <div class="container about-container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 about-content">
+          <h2 class="about-title">Empresa</h2>
+          <p class="about-text">
+            Somos una empresa certificada ISO 9001 en Calidad de Servicio. Tenemos Obras de Ingeniería, tenemos Infraestructura realizadas a lo largo de todo el país.
+          </p>
+          <p class="about-text">
+           Realizamos servicios de proyecto, construcción,  mantenimiento y reformas de todo tipo de edificaciones. Avalados por nuestra amplia trayectoria de 30 años al servicio de la misma.
+            Nuestro sistema de trabajo directo y personalizado, nos permite cumplir con cada uno de los objetivos propuestos, basándonos en ciertos principios que guían nuestro accionar: respeto absoluto por la necesidad del cliente, costos competitivos, cumplimiento de plazos, respuesta rápida y efectiva, y calidad del producto ofrecido.
+          </p>
+          <p class="about-text">
+            nos destacamos por los servicios y la calidad de nuestros trabajos.cuenta con el Recurso Humano, las herramientas, maquinarias y el know-how para desempeñar una excelente labor
+          </p>
+        </div>
+      </div>
+    </div>
+      </div>
+  </section>
+  <!--/about-us-->
+
+  <footer>
+    <div class="container">
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <h4>About Us</h4>
+        <p>SystemDNS is tellus ac cursus commodo, mauesris condime ntum nibh, ut fermentum mas justo sitters.</p>
+        <div class="contact-info">
+          <ul>
+            <li><i class="fa fa-home fa"></i>Suite 54 Elizebth Street, Victoria State Newyork, USA </li>
+            <li><i class="fa fa-phone fa"></i> +38 000 129900</li>
+            <li><i class="fa fa-envelope fa"></i> info@domain.net</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <div class="text-center">
+          <h4>Photo Gallery</h4>
+          <ul class="sidebar-gallery">
+            <li><a href="#"><img src="img/gallery1.png" alt="" /></a></li>
+            <li><a href="#"><img src="img/gallery2.png" alt="" /></a></li>
+            <li><a href="#"><img src="img/gallery3.png" alt="" /></a></li>
+            <li><a href="#"><img src="img/gallery4.png" alt="" /></a></li>
+            <li><a href="#"><img src="img/gallery5.png" alt="" /></a></li>
+            <li><a href="#"><img src="img/gallery6.png" alt="" /></a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+        <div class="">
+          <h4>Newsletter Registration</h4>
+          <p>Subscribe to ProOnliPc to receive the latest ProOnliPc news via email. You may unsubscribe from this service at any time</p>
+          <div class="btn-gamp">
+            <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Enter Email">
+          </div>
+          <div class="btn-gamp">
+            <a type="submit" class="btn btn-default">Subscribe</a>
+          </div>
+
+        </div>
+
+      </div>
+  </footer>
+
+  <div class="sub-footer">
+    <div class="container">
+      <div class="social-icon">
+        <div class="col-md-4">
+          <ul class="social-network">
+            <li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#" class="gplus tool-tip" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="#" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#" class="ytube tool-tip" title="You Tube"><i class="fa fa-youtube-play"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-md-offset-4">
+        <div class="copyright">
+          &copy; ProOnliPc Theme. All Rights Reserved.
+          <div class="credits">
+          
+            <a href="https://www.youtube.com/channel/UCDH0DJaVLkCDtl_YN9hhByw?view_as=subscriber">ProOnliPc</a> by <a href="https://www.youtube.com/channel/UCDH0DJaVLkCDtl_YN9hhByw?view_as=subscriber">ProOnliPc</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="js/jquery.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script>
+    wow = new WOW({}).init();
+  </script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,468 @@
+<!DOCTYPE html> 
+
+<html lang="en"> 
+
+<head> 
+
+  <meta charset="utf-8"> 
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+ 
+
+  <title>Lepuri kod</title> 
+
+
+
+  <!-- Bootstrap --> 
+
+  <link href="css/bootstrap.min.css" rel="stylesheet"> 
+
+  <link rel="stylesheet" href="css/font-awesome.min.css"> 
+
+  <link rel="stylesheet" href="css/animate.css"> 
+
+  <link href="css/animate.min.css" rel="stylesheet"> 
+
+  <link href="css/style.css" rel="stylesheet" /> 
+
+</head> 
+
+ 
+
+<body> 
+
+<?php include"menu.html" ?> 
+
+ 
+
+  <div class="slider"> 
+
+    <div id="about-slider"> 
+
+      <div id="carousel-slider" class="carousel slide" data-ride="carousel"> 
+
+ 
+
+        <ol class="carousel-indicators visible-xs"> 
+
+          <li data-target="#carousel-slider" data-slide-to="0" class="active"></li> 
+
+          <li data-target="#carousel-slider" data-slide-to="1"></li> 
+
+          <li data-target="#carousel-slider" data-slide-to="2"></li> 
+
+        </ol> 
+
+ 
+
+        <div class="carousel-inner"> 
+
+          <div class="item active"> 
+
+            <img src="img/slide.jpg" class="img-responsive" alt=""> 
+
+            <div class="carousel-caption"> 
+
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s"> 
+
+                <h2><span>Conocé nuestros hosting</span></h2> 
+
+              </div> 
+
+              <div class="col-md-10 col-md-offset-1"> 
+
+                <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s"> 
+
+                  <p>Plan de hosting y dominio</p> 
+
+                </div> 
+
+              </div> 
+
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.9s"> 
+
+                <form class="form-inline"> 
+
+                  <div class="form-group"> 
+
+                    <button type="livedemo" name="Live Demo" class="btn btn-primary btn-lg" required="required">Contratar hosting</button> 
+
+                  </div> 
+
+                  <div class="form-group"> 
+
+                    <button type="getnow" name="Get Now" class="btn btn-primary btn-lg" required="required">Contratar dominio</button> 
+
+                  </div> 
+
+                </form> 
+
+              </div> 
+
+            </div> 
+
+          </div> 
+
+          <div class="item"> 
+
+            <img src="img/slide2.jpg" class="img-responsive" alt=""> 
+
+            <div class="carousel-caption"> 
+
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="1.0s"> 
+
+                <h2>Webs para empresas y negocios</h2> 
+
+              </div> 
+
+              <div class="col-md-10 col-md-offset-1"> 
+
+                <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s"> 
+
+                  <p>Diseñamos tu web profesional desde 0</p> 
+
+                </div> 
+
+              </div> 
+
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="1.6s"> 
+
+                <form class="form-inline"> 
+
+                  <div class="form-group"> 
+
+                    <button type="livedemo" name="purchase" class="btn btn-primary btn-lg" required="required">Ver ejemplos</button> 
+
+                  </div> 
+
+                  <div class="form-group"> 
+
+                    <button type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Contratar ahora</button> 
+
+                  </div> 
+
+                </form> 
+
+              </div> 
+
+            </div> 
+
+          </div> 
+
+          <div class="item"> 
+
+            <img src="img/slide3.jpg" class="img-responsive" alt=""> 
+
+            <div class="carousel-caption"> 
+
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s"> 
+
+                <h2>Nos enfocamos en cumplir tus objetivos</h2> 
+
+              </div> 
+
+              <div class="col-md-10 col-md-offset-1"> 
+
+                <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s"> 
+
+                  <p>Publicidad y posicionamiento</p> 
+
+                </div> 
+
+              </div> 
+
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.9s"> 
+
+                <form class="form-inline"> 
+
+                  <div class="form-group"> 
+
+                    <button type="livedemo" name="purchase" class="btn btn-primary btn-lg" required="required">Realizar publicidad</button> 
+
+                  </div> 
+
+                  <div class="form-group"> 
+
+                    <button type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Posicionar mi web</button> 
+
+                  </div> 
+
+                </form> 
+
+              </div> 
+
+            </div> 
+
+          </div> 
+
+        </div> 
+
+ 
+
+        <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev"> 
+
+<i class="fa fa-angle-left"></i> 
+
+</a> 
+
+ 
+
+        <a class=" right carousel-control hidden-xs" href="#carousel-slider" data-slide="next"> 
+
+<i class="fa fa-angle-right"></i> 
+
+</a> 
+
+      </div> 
+
+      <!--/#carousel-slider--> 
+
+    </div> 
+
+    <!--/#about-slider--> 
+
+  </div> 
+
+  <!--/#slider--> 
+
+ 
+
+  <div class="about"> 
+
+    <div class="container"> 
+
+      <div class="text-center"> 
+
+ 
+
+           <h1 style="color: black">Planes webs prediseñados</h1> 
+
+    <p style="color: black; font-size: 18px;">Si usted quiere una web personalizada envié un e-mail en la sesión <a href="contact.php" style="color: orangered">!contacto!</a></p> 
+
+<?php include "pketes.html" ?> 
+
+      </div> 
+
+    </div> 
+
+  </div> 
+
+  <hr> 
+
+ 
+
+  <div class="services"> 
+
+    <div class="container"> 
+
+      <div class="text-center"> 
+
+        <h1>Nos especializamos en</h1> 
+
+      </div> 
+
+      <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms"> 
+
+        <i class="fa fa-heart-o" style="color: #222831;"></i> 
+
+        <h3 style="color: ">Estilo full Responsive</h3> 
+
+        <p style="color: #222831;">Adaptamos tu web a cualquier dispositivo, celulares y tablets. Esto ayuda a posicionar al sitio en los buscadores y obtener mejores resultados en la búsqueda</p> 
+
+      </div> 
+
+      <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms"> 
+
+        <i class="fa fa-code" style="color: #222831;"></i> 
+
+        <h3>Back-End </h3> 
+
+        <p style="color: #222831;">El backend sirve para hacer el esqueleto de la página, todo lo que es texto e imágenes, etc. Con los lenguajes Html5, Css3, JavaScript, Bootstrap</p> 
+
+      </div> 
+
+      <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms"> 
+
+        <i class="fa fa-hdd-o" aria-hidden="true" style="color: #222831;" ></i> 
+
+        <h3>Front-End</h3> 
+
+        <p style="color: #222831;">En cambio el Frontend, es la parte detrás de la página, la cual maneja la base de datos y demás, acá es donde iría la parte del formulario las compras etc. Los lenguajes utilizados, Php, MySql, Jquerys, NodeJs </p> 
+
+      </div> 
+
+      <div class="col-md-3 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms"> 
+
+        <i class="fa fa-gear" style="color: #222831;"></i> 
+
+        <h3>Marketing y publicidad</h3> 
+
+        <p style="color: #222831;">Realizamos campañas de publicidad en diferentes plataformas, YouTube, Facebook, Instagram, GoogleAds. Esto genera más clic o ventas dependiendo del objetivo que tengas con la publicidad</p> 
+
+      </div> 
+
+    </div> 
+
+  </div> 
+
+  <section class="action"> 
+
+    <div class="container"> 
+
+      <div class="left-text hidden-xs"> 
+
+        <h4>Contamos con Hosting y dominio</h4> 
+
+        <p><em>Puedes tener tu web completa y en línea gracias a nuestro hosting </em><br><br> Contrátanos como tus programadores y obtén un descuento en el hosting y dominio en el primer mes. 
+       Pregunta por nuestros paquetes <a href="#pkts">prediseñados</a></p> 
+
+      </div> 
+
+      <div class="right-image hidden-xs"></div> 
+
+    </div> 
+
+  </section> 
+
+  <footer> 
+
+    <div class="container"> 
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms"> 
+
+        <h4>Nosotros</h4> 
+
+        <p>Sáquese todas sus dudas, contáctanos ya sea vía email o llamada, también puedes contactarnos por WhatsApp</p> 
+
+        <div class="contact-info"> 
+
+          <ul> 
+
+            <li><i class="fa fa-home fa"></i>Bahía Blanca, Prov: Buenos Aires</li> 
+
+            <li><i class="fa fa-phone fa"></i>+54 291 436 9407</li> 
+
+            <li><i class="fa fa-envelope fa"></i>aronelia699@gmail.com</li> 
+
+          </ul> 
+
+        </div> 
+
+      </div> 
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms"> 
+
+        <div class="text-center"> 
+
+          <h4>Fotos</h4> 
+
+          <ul class="sidebar-gallery"> 
+
+            <li><a href="#"><img src="img/gallery1.png" alt="" /></a></li> 
+
+            <li><a href="#"><img src="img/gallery2.png" alt="" /></a></li> 
+
+            <li><a href="#"><img src="img/gallery3.png" alt="" /></a></li> 
+
+            <li><a href="#"><img src="img/gallery4.png" alt="" /></a></li> 
+
+            <li><a href="#"><img src="img/gallery5.png" alt="" /></a></li> 
+
+            <li><a href="#"><img src="img/gallery6.png" alt="" /></a></li> 
+
+          </ul> 
+
+        </div> 
+
+      </div> 
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms"> 
+
+        <div class=""> 
+
+          <h4>Envíanos un email</h4> 
+
+          <p>Te enviaremos ofertas y seminarios!</p> 
+
+          <div class="btn-gamp"> 
+
+            <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Enter Email"> 
+
+          </div> 
+
+          <div class="btn-gamp"> 
+            <a type="submit" class="btn btn-default">Suscríbete</a> 
+          </div> 
+
+        </div> 
+
+      </div> 
+
+    </div> 
+
+  </footer> 
+  <div class="sub-footer"> 
+
+    <div class="container"> 
+
+      <div class="social-icon"> 
+
+        <div class="col-md-4"> 
+
+          <ul class="social-network"> 
+
+            <li><a href="http://www.facebook.com/falconmasters" target="_blank" class="fa fa-facebook"></a></li> 
+
+            <li><a href="http://www.twitter.com/falconmasters" target="_blank" class="fa fa-instagram"></a></li> 
+
+            <li><a href="http://www.googleplus.com/falconmasters" target="_blank" class="fa fa-google-plus"></a></li> 
+
+          </ul> 
+
+        </div> 
+
+      </div> 
+
+      <div class="col-md-4 col-md-offset-4"> 
+
+        <div class="copyright"> 
+
+          &copy; LepuriKod Prod. Derechos reservados. 
+
+          <div class="credits"> 
+            <a href="https://www.youtube.com/channel/UCDH0DJaVLkCDtl_YN9hhByw?view_as=subscriber">LepuriKod Temas</a> by <a href="https://www.youtube.com/channel/UCDH0DJaVLkCDtl_YN9hhByw?view_as=subscriber">LepuriKod</a> 
+          </div> 
+
+        </div> 
+
+      </div> 
+
+    </div> 
+
+  </div> 
+
+  <script src="js/jquery.js"></script> 
+
+  <script src="js/bootstrap.min.js"></script> 
+
+  <script src="js/wow.min.js"></script> 
+
+  <script> 
+
+    wow = new WOW({}).init(); 
+
+  </script> 
+
+</body> 
+
+ 
+
+</html> 
+
+
+<!-- ----------------------------------------------------------------------------
+  ACA EMPIZA EL VIEJO HTML QUE TE LA INCATODAAAAAAAAAAAAAAA---------------------
+  ---------------------AA-------------------------------------------------------->
